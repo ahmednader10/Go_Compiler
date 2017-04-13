f2a7f2122e1edfd6c08241fe89e4183d35b5f418
Nader <nadera2@emc.com>
2017-04-13 13:40:15 +0200

    test

--- src/MLA.java
+++ src/MLA.java
@@ -17,7 +17,7 @@ import java.util.Stack;
  * Output file will be automatically overwritten if exists.
  * 
  */
-//test
+//test 1
 public class MLA {
 	public static void main(String[] args) {
 		String inFile = "Go10.in";
